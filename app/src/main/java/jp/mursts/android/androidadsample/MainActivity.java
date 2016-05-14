7894d4b2d67410f22d10f889c857d82ce64dd9f6
mursts <mursts@gmail.com>
2016-05-15 00:29:48 +0900

    不要なものを削除

--- app/src/main/java/jp/mursts/android/androidadsample/MainActivity.java
+++ app/src/main/java/jp/mursts/android/androidadsample/MainActivity.java
@@ -14,26 +14,12 @@ import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String myDevice = "";
-
-    private AdView mAdView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
